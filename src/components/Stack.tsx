f68b51d9fda1d474e4175016b57afdb78261b460
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-04-13 15:08:15 +0100

    Contact server and style page a bit

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,6 +1,23 @@
-import { For } from "solid-js";
+import { For, createSignal, onMount } from "solid-js";
+import "./Stack.css";
+
+type Argument = {
+	argument: string;
+};
 
 export default function Stack() {
-	const args = ["Argument 1", "Argument 2"];
-	return <For each={args}>{(arg) => <p>{arg}</p>}</For>;
+	const [arg3, setArg3] = createSignal("");
+	const args = () => ["Argument 1", "Argument 2", arg3()];
+	onMount(async () => {
+		const res = await fetch("http://127.0.0.1:3000/title");
+		const argument = (await res.json()) as Argument;
+		setArg3(argument.argument);
+	});
+	return (
+		<div class="stack">
+			<For each={args()} fallback={<p>Loading…</p>}>
+				{(arg) => <div>{arg}</div>}
+			</For>
+		</div>
+	);
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,18 @@
 import { Title } from "@solidjs/meta";
 import Counter from "~/components/Counter";
 import Stack from "~/components/Stack";
+import "./index.css";
 
 export default function Home() {
 	return (
 		<main>
-			<Title>Hello World</Title>
-			<h1>Hello world!</h1>
-			<Counter />
+			<Stack />
 			<div>
-				<Stack />
+				<Title>Hello World</Title>
+				<h1>Hello world!</h1>
+				<Counter />
 			</div>
+			<Stack />
 		</main>
 	);
 }
