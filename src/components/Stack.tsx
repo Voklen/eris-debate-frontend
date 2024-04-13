e425a0efa67fed8f5e6da9a1a9d89473bb49ea71
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-04-13 15:53:52 +0100

    Accept argument array from backend

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -6,17 +6,16 @@ type Argument = {
 };
 
 export default function Stack() {
-	const [arg3, setArg3] = createSignal("");
-	const args = () => ["Argument 1", "Argument 2", arg3()];
+	const [args, setArgs] = createSignal<Argument[]>([]);
 	onMount(async () => {
 		const res = await fetch("http://127.0.0.1:3000/title");
-		const argument = (await res.json()) as Argument;
-		setArg3(argument.argument);
+		const argument = (await res.json()) as Argument[];
+		setArgs(argument);
 	});
 	return (
 		<div class="stack">
 			<For each={args()} fallback={<p>Loading…</p>}>
-				{(arg) => <div>{arg}</div>}
+				{(arg) => <div>{arg.argument}</div>}
 			</For>
 		</div>
 	);
