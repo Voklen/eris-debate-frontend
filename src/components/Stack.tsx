02e1b117650cf05b4a65f07ac30a769e70f5b7f4
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-04-17 15:54:06 +0100

    Display title with arguments

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -3,12 +3,14 @@ import "./Stack.css";
 import type { Argument } from "~/routes";
 
 type Props = {
+	title: string;
 	args: Argument[];
 };
 
 export default function Stack(props: Props) {
 	return (
 		<div class="stack">
+			<h2>{props.title}</h2>
 			<For each={props.args} fallback={<p>Loading…</p>}>
 				{(arg) => <div>{arg.body}</div>}
 			</For>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,29 +10,33 @@ export type Argument = {
 };
 
 type Topic = {
-	for: Argument[];
-	against: Argument[];
+	for: { title: string; arguments: Argument[] };
+	against: { title: string; arguments: Argument[] };
 };
 
 export default function Home() {
 	const [forArgs, setForArgs] = createSignal<Argument[]>([]);
 	const [againstArgs, setAgainstArgs] = createSignal<Argument[]>([]);
+	const [forTitle, setForTitle] = createSignal<string>("");
+	const [againstTitle, setAgainstTitle] = createSignal<string>("");
 
 	onMount(async () => {
 		const res = await fetch("http://127.0.0.1:9000/topic");
 		const topic = (await res.json()) as Topic;
-		setForArgs(topic.for);
-		setAgainstArgs(topic.against);
+		setForTitle(topic.for.title);
+		setAgainstTitle(topic.against.title);
+		setForArgs(topic.for.arguments);
+		setAgainstArgs(topic.against.arguments);
 	});
 	return (
 		<main>
-			<Stack args={forArgs()} />
+			<Stack args={againstArgs()} title={forTitle()} />
 			<div>
 				<Title>Hello World</Title>
 				<h1>Hello world!</h1>
 				<Counter />
 			</div>
-			<Stack args={againstArgs()} />
+			<Stack args={forArgs()} title={againstTitle()} />
 		</main>
 	);
 }
