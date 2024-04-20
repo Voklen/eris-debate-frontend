ab9796dfa56a6888468591f11ff1b5a1e0a6759d
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-04-20 11:16:08 +0100

    Create functionality to add argument

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,6 +1,8 @@
 import { For } from "solid-js";
 import styles from "./Stack.module.css";
 import type { Argument } from "~/routes";
+import { Portal } from "solid-js/web";
+import Popup from "./Popup";
 
 type Props = {
 	title: string;
@@ -16,12 +18,20 @@ export default function Stack(props: Props) {
 				{(arg) => {
 					const newLocal = () => props.onArgSelected(arg.id);
 					return (
-						<div onClick={newLocal} onKeyDown={newLocal}>
+						<div onClick={newLocal} onKeyDown={newLocal} class={styles.arg}>
 							{arg.body}
 						</div>
 					);
 				}}
 			</For>
+			<div class={styles.add}>
+				+
+				<Popup>
+					<h2>Create an argument</h2>
+					<h3>{props.title}</h3>
+					<textarea />
+				</Popup>
+			</div>
 		</div>
 	);
 }
