d375164d24e6eadf0159c1e0e6126fd3e9c98208
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-05-19 10:59:10 +0100

    Add profile page with logout functionality

---
+++ src/routes/profile/index.tsx
@@ -0,0 +1,47 @@
+import { Show, createSignal, onMount } from "solid-js";
+import { generateProfilePic } from "~/utils/profilePic";
+import styles from "./profile.module.css";
+
+export default function Home() {
+	const [username, setUsername] = createSignal<string | null>("");
+	onMount(() => setUsername(localStorage.getItem("username")));
+
+	return (
+		<>
+			<Show when={username()}>
+				{/* When signed in */}
+				<a href="/profile" id={styles.profileUsername}>
+					<img
+						src={generateProfilePic(username()!)}
+						alt="Profile"
+						height={50}
+					/>
+					{username()}
+				</a>
+			</Show>
+			<button type="button" onClick={logout}>
+				Log out
+			</button>
+		</>
+	);
+}
+
+function logout() {
+	localStorage.removeItem("username");
+	sendLogoutRequest();
+}
+
+async function sendLogoutRequest() {
+	const res = await fetch("http://127.0.0.1:9000/logout", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		credentials: "include",
+	});
+	switch (res.status) {
+		case 200:
+			return;
+	}
+	throw new Error(`Unknown error ${res.status}: ${await res.text()}`);
+}
