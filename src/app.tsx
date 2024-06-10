51da60e5c45743d0b6bdfe4b715585e2bdafe777
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-06-11 00:08:37 +0100

    Persist Navbar login state with localstorage

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { MetaProvider, Title } from "@solidjs/meta";
-import { type RouteSectionProps, Router } from "@solidjs/router";
+import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { type Component, Suspense } from "solid-js";
+import { type JSX, Suspense } from "solid-js";
 import "./app.css";
 import { Toaster } from "solid-toast";
 import Navbar from "./components/Navbar";
@@ -9,19 +9,25 @@ import { AuthProvider } from "./providers/auth";
 
 export default function App() {
 	return (
-		<Router root={root}>
+		<Router root={Root}>
 			<FileRoutes />
 		</Router>
 	);
 }
 
-const root: Component<RouteSectionProps> = (props) => (
-	<MetaProvider>
-		<AuthProvider>
-			<Title>SolidStart - Basic</Title>
-			<Navbar />
-			<Suspense>{props.children}</Suspense>
-			<Toaster position="bottom-center" />
-		</AuthProvider>
-	</MetaProvider>
-);
+type Props = {
+	children?: JSX.Element;
+};
+
+function Root(props: Props) {
+	return (
+		<MetaProvider>
+			<AuthProvider>
+				<Title>SolidStart - Basic</Title>
+				<Navbar />
+				<Suspense>{props.children}</Suspense>
+				<Toaster position="bottom-center" />
+			</AuthProvider>
+		</MetaProvider>
+	);
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
-import { Match, Switch } from "solid-js";
+import { Show, onMount } from "solid-js";
 import { useAuth } from "~/providers/auth";
 import { generateProfilePic } from "~/utils/profilePic";
+import type { User } from "~/utils/types";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-	const [user, _] = useAuth();
+	const [user, { login }] = useAuth();
+
+	onMount(() => {
+		const user = getUser();
+		if (user === null) return;
+		return login(user);
+	});
 
 	return (
 		<header class={styles.header}>
@@ -13,33 +20,38 @@ export default function Navbar() {
 				<a href="/about">About</a>
 			</nav>
 			<div class={styles.headerEnd}>
-				<Switch>
-					<Match when={user() === null}>
-						{/* When not signed in */}
-						<>
-							<a href="/login">
-								<button type="button" id={styles.login}>
-									Login
-								</button>
-							</a>
-							<a href="/signup">
-								<button type="button">Sign up</button>
-							</a>
-						</>
-					</Match>
-					<Match when={user()}>
-						{/* When signed in */}
-						<a href="/profile" id={styles.profileUsername}>
-							{user()?.username}
-							<img
-								src={generateProfilePic(user()!.username)}
-								alt="Profile"
-								height={50}
-							/>
-						</a>
-					</Match>
-				</Switch>
+				<Show when={user()} fallback={<LoginButtons />}>
+					{/* When signed in */}
+					<a href="/profile" id={styles.profileUsername}>
+						{user()?.username}
+						<img
+							src={generateProfilePic(user()!.username)}
+							alt="Profile"
+							height={50}
+						/>
+					</a>
+				</Show>
 			</div>
 		</header>
 	);
+}
+
+function LoginButtons() {
+	return (
+		<>
+			<a href="/login">
+				<button type="button" id={styles.login}>
+					Login
+				</button>
+			</a>
+			<a href="/signup">
+				<button type="button">Sign up</button>
+			</a>
+		</>
+	);
+}
+
+function getUser(): User | null {
+	const username = localStorage.getItem("username");
+	return username ? { username } : null;
 }
