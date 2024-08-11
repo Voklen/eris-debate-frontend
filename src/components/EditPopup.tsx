b802ac27983405312f1bee722ab1a5663a557565
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-08-11 22:16:34 +0100

    Add proper argument editing functionality

--- src/components/EditPopup.tsx
+++ src/components/EditPopup.tsx
@@ -1,4 +1,6 @@
 import { createSignal } from "solid-js";
+import toast from "solid-toast";
+import { backendURL } from "~/utils/config";
 import type { Argument } from "~/utils/types";
 import styles from "./EditPopup.module.css";
 import Popup from "./Popup";
@@ -10,7 +12,18 @@ type Props = {
 
 export default function EditPopup(props: Props) {
 	const [text, setText] = createSignal(props.arg.body);
-	const submit = () => {};
+	const submit = async () => {
+		try {
+			const requestFuture = sendPutToServer(props.arg.id, text());
+			props.closePopup();
+			// Await request to catch errors
+			await requestFuture;
+		} catch (e) {
+			if (e instanceof Error) {
+				toast.error(e.message);
+			}
+		}
+	};
 
 	return (
 		<Popup closePopup={props.closePopup}>
@@ -27,4 +40,19 @@ export default function EditPopup(props: Props) {
 			</form>
 		</Popup>
 	);
+}
+
+async function sendPutToServer(arg_id: number, text: string) {
+	const requestBody = { arg_id, body: text };
+	const res = await fetch(`${backendURL}/arguments`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		credentials: "include",
+		body: JSON.stringify(requestBody),
+	});
+	if (res.status !== 200) {
+		throw new Error(`${res.status}: ${await res.text()}`);
+	}
 }
